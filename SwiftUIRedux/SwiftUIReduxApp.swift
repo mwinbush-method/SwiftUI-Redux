afff167186a58b597059e737afed4ae158414084
Morgan Winbush <morgan.winbush@method.com>
2021-07-28 14:40:26 -0400

    Initial Commit

---
+++ SwiftUIRedux/SwiftUIReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReduxApp.swift
+//  SwiftUIRedux
+//
+//  Created by Morgan Winbush on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
